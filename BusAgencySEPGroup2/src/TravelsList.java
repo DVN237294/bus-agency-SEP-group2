1f6462497f42d3a22b4c8f6c65093e176ba5b2c1
DVN <237294@via.dk>
2016-12-01 15:29:17 +0100

    Implemented public Chauffeur[] listAvailableChauffeurs(...)

--- BusAgencySEPGroup2/src/TravelsList.java
+++ BusAgencySEPGroup2/src/TravelsList.java
@@ -6,40 +6,47 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class TravelsList
 {
-   private ArrayList<Travel> travels;
+	private ArrayList<Travel> travels;
 
-   public TravelsList()
-   {
-      travels = new ArrayList<Travel>();
-   }
+	public TravelsList()
+	{
+		travels = new ArrayList<Travel>();
+	}
 
-   // public addTourPassenger() pointless
-   
-   public void addTravel(Travel travel) {
-      travels.add(travel);
-   }
-   
-   public void deleteTravel(Travel travel) {
-      travels.remove(travel);
-   }
+	// public addTourPassenger() pointless
 
-   public boolean hasReservationFor(Bus what, LocalDateTime startDate, LocalDateTime endDate)
-   {
-      for(Travel travel : travels)
-      {
-    	  if(travel.getBus().equals(what) && travel.reservationOverlaps(startDate, endDate))
-    		  return true;
-      }
-      return false;
-   }
+	public void addTravel(Travel travel)
+	{
+		travels.add(travel);
+	}
 
-   public boolean hasReservationFor(Chauffeur what, Date startDate, Date endDate)
-   {
-      throw new NotImplementedException(); // hmm, what did i want to do here..?
-   }
+	public void deleteTravel(Travel travel)
+	{
+		travels.remove(travel);
+	}
 
-   public int getNumberOftravels()
-   {
-      return travels.size();
-   }
+	public boolean hasReservationFor(Bus what, LocalDateTime startDate, LocalDateTime endDate)
+	{
+		for (Travel travel : travels)
+		{
+			if (travel.getBus().equals(what) && travel.reservationOverlaps(startDate, endDate))
+				return true;
+		}
+		return false;
+	}
+
+	public boolean hasReservationFor(Chauffeur what, LocalDateTime startDate, LocalDateTime endDate)
+	{
+		for (Travel travel : travels)
+		{
+			if (travel.getChauffeur().equals(what) && travel.reservationOverlaps(startDate, endDate))
+				return true;
+		}
+		return false;
+	}
+
+	public int getNumberOftravels()
+	{
+		return travels.size();
+	}
 }
