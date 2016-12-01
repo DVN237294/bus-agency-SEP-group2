f38dc5dc710a17dde17f7c3e503d48ce3956359e
Afonso <Afonso@10.52.225.228>
2016-12-01 12:34:24 +0100

    Formatted code and added "deleteBus" method

--- BusAgencySEPGroup2/src/BusList.java
+++ BusAgencySEPGroup2/src/BusList.java
@@ -2,21 +2,27 @@ import java.util.ArrayList;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-
 public class BusList
 {
-	private ArrayList<Bus> buses;
-	
-	public BusList()
-	{
-		buses = new ArrayList<Bus>();
-	}
-	public void addBus(Bus bus)
-	{
-		buses.add(bus);
-	}
-	public Bus[] getBusses(int minCapacity)
-	{
-		throw new NotImplementedException();
-	}
+   private ArrayList<Bus> buses;
+
+   public BusList()
+   {
+      buses = new ArrayList<Bus>();
+   }
+
+   public void addBus(Bus bus)
+   {
+      buses.add(bus);
+   }
+
+   public void deleteBus(Bus bus)
+   {
+      buses.remove(bus);
+   }
+
+   public Bus[] getBusses(int minCapacity)
+   {
+      throw new NotImplementedException();
+   }
 }
