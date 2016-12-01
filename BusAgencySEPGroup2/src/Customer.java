b7954ef4513430c71b50dbee5a8c4bd78a69c068
Afonso <Afonso@10.52.225.228>
2016-12-01 14:07:59 +0100

    Added private variable Arraylist<PassengerList>, "addPassengerList" and "deletePassengerList" methods

--- BusAgencySEPGroup2/src/Customer.java
+++ BusAgencySEPGroup2/src/Customer.java
@@ -1,12 +1,25 @@
+import java.util.ArrayList;
+
 public class Customer
 {
    private String name;
    private int phoneNumber;
+   private ArrayList<PassengerList> passengerLists;
 
    public Customer(String name, int phoneNumber)
    {
       this.name = name;
       this.phoneNumber = phoneNumber;
+   }
+   
+   public void addPassengerList(PassengerList passengerList)
+   {
+      passengerLists.add(passengerList);
+   }
+   
+   public void deletePassengerList(PassengerList passengerList)
+   {
+      passengerLists.remove(passengerList);
    }
    
    public String getName()
