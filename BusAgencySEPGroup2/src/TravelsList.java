3fd067bd29c68f2b6462bc62e4eb0422d5b65825
Afonso <Afonso@10.52.225.228>
2016-12-01 11:47:10 +0100

    Added getter for the number of travels

--- BusAgencySEPGroup2/src/TravelsList.java
+++ BusAgencySEPGroup2/src/TravelsList.java
@@ -3,24 +3,29 @@ import java.util.Date;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-
 public class TravelsList
 {
-	private ArrayList<Travel> travels;
-	
-	public TravelsList()
-	{
-		travels = new ArrayList<Travel>();
-	}
-	
-	//public addTourPassenger() pointless
-	
-	public boolean hasReservation(Bus what, Date startDate, Date endDate)
-	{
-		throw new NotImplementedException(); //hmm, what did i want to do here..?
-	}
-	public boolean hasReservation(Chauffeur what, Date startDate, Date endDate)
-	{
-		throw new NotImplementedException(); //hmm, what did i want to do here..?
-	}
+   private ArrayList<Travel> travels;
+
+   public TravelsList()
+   {
+      travels = new ArrayList<Travel>();
+   }
+
+   // public addTourPassenger() pointless
+
+   public boolean hasReservation(Bus what, Date startDate, Date endDate)
+   {
+      throw new NotImplementedException(); // hmm, what did i want to do here..?
+   }
+
+   public boolean hasReservation(Chauffeur what, Date startDate, Date endDate)
+   {
+      throw new NotImplementedException(); // hmm, what did i want to do here..?
+   }
+
+   public int getNumberOftravels()
+   {
+      return travels.size();
+   }
 }
