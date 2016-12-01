2dfb236d097d54496dce95942b04f22440233a92
Afonso <Afonso@10.52.225.228>
2016-12-01 12:58:02 +0100

    Added private variables for the BusList, ChauffeurList, CustomerList and TravelsList classes. Also added a Constructor

--- BusAgencySEPGroup2/src/TravelAgency.java
+++ BusAgencySEPGroup2/src/TravelAgency.java
@@ -2,15 +2,31 @@ import java.util.Date;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-
 public class TravelAgency
 {
-	public Bus[] listAvailableBusses(Date startDate, Date endDate, int minCapacity)
-	{
-		throw new NotImplementedException();
-	}
-	public Chauffeur[] listAvailableChauffeurs(Date startDate, Date endDate)
-	{
-		throw new NotImplementedException();
-	}
+
+   private BusList busList;
+   private ChauffeurList chauffeurList;
+   private CustomerList customerList;
+   private TravelsList travelsList;
+
+   public TravelAgency(BusList busList, ChauffeurList chauffeurList,
+         CustomerList customerList, TravelsList travelsList)
+   {
+      this.busList = busList;
+      this.chauffeurList = chauffeurList;
+      this.customerList = customerList;
+      this.travelsList = travelsList;
+   }
+
+   public Bus[] listAvailableBusses(Date startDate, Date endDate,
+         int minCapacity)
+   {
+      throw new NotImplementedException();
+   }
+
+   public Chauffeur[] listAvailableChauffeurs(Date startDate, Date endDate)
+   {
+      throw new NotImplementedException();
+   }
 }
