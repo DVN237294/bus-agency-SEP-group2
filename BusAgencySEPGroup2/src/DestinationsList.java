7652060bb2fa4a6f2c57dbc75516c27190712de6
Afonso <Afonso@10.52.224.66>
2016-12-05 16:18:18 +0100

    Formatted code

--- BusAgencySEPGroup2/src/DestinationsList.java
+++ BusAgencySEPGroup2/src/DestinationsList.java
@@ -2,19 +2,22 @@ import java.util.ArrayList;
 
 public class DestinationsList
 {
-	private ArrayList<String> destinations;
-	public DestinationsList()
-	{
-		destinations = new ArrayList<String>();
-	}
-	public void add(String destination)
-	{
-		destinations.add(destination);
-	}
-	public String[] getAllDestinations()
-	{
-		String[] temp = new String[destinations.size()];
-		destinations.toArray(temp);
-		return temp;
-	}
+   private ArrayList<String> destinations;
+
+   public DestinationsList()
+   {
+      destinations = new ArrayList<String>();
+   }
+
+   public void add(String destination)
+   {
+      destinations.add(destination);
+   }
+
+   public String[] getAllDestinations()
+   {
+      String[] temp = new String[destinations.size()];
+      destinations.toArray(temp);
+      return temp;
+   }
 }
