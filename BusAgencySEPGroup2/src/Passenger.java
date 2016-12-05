68cc8cf59b1925b2743a87d47dca87404e00f17e
Afonso <Afonso@10.52.226.236>
2016-12-05 13:36:59 +0100

    Formatted code

--- BusAgencySEPGroup2/src/Passenger.java
+++ BusAgencySEPGroup2/src/Passenger.java
@@ -2,46 +2,47 @@ import java.time.LocalDateTime;
 
 public class Passenger
 {
-	private String name;
-	private String phoneNumber;
-	private LocalDateTime birthday;
-	private String email;
-	
-	public Passenger(String name, String phoneNumber, LocalDateTime birthday)
-	{
-		this.name = name;
-		this.phoneNumber = phoneNumber;
-		this.birthday = birthday;
-		this.email = null;
-	}
-	
+   private String name;
+   private String phoneNumber;
+   private LocalDateTime birthday;
+   private String email;
 
-	public String getName()
-	{
-		return name;
-	}
-	public String getPhoneNumber()
-	{
-		return phoneNumber;
-	}
-	
-	public String getEmail()
-	{
-		return email;
-	}
+   public Passenger(String name, String phoneNumber, LocalDateTime birthday)
+   {
+      this.name = name;
+      this.phoneNumber = phoneNumber;
+      this.birthday = birthday;
+      this.email = null;
+   }
 
-	public void setEmail(String email)
-	{
-		this.email = email;
-	}
+   public String getName()
+   {
+      return name;
+   }
 
-	public LocalDateTime getBirthday()
-	{
-		return birthday;
-	}
-	
-	public boolean equals(Passenger other)
-	{
-		return name.equals(other.getName()) && phoneNumber == other.getPhoneNumber();
-	}
+   public String getPhoneNumber()
+   {
+      return phoneNumber;
+   }
+
+   public String getEmail()
+   {
+      return email;
+   }
+
+   public void setEmail(String email)
+   {
+      this.email = email;
+   }
+
+   public LocalDateTime getBirthday()
+   {
+      return birthday;
+   }
+
+   public boolean equals(Passenger other)
+   {
+      return name.equals(other.getName())
+            && phoneNumber == other.getPhoneNumber();
+   }
 }
