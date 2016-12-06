2dcd4aee05ffd67e8da692d035fb33a0a3590691
DVN <237294@via.dk>
2016-12-06 11:43:12 +0100

    Tours GUI Pane

---
+++ BusAgencySEPGroup2/src/ToursPane.java
@@ -0,0 +1,30 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+public class ToursPane extends JPanel
+{
+	private TravelAgency agency;
+	public ToursPane(TravelAgency agency)
+	{
+		this.agency = agency;
+		// Tour pane
+		JPanel toursNorthPanel = new JPanel();
+		toursNorthPanel.setBorder(BorderFactory.createTitledBorder("Search"));
+		toursNorthPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		String[] destinations = agency.getAllDestinations();
+		JExtendedComboBox<String> destiBox = new JExtendedComboBox<String>(destinations);
+		destiBox.setPrototypeDisplayValue("Destination");
+		destiBox.setDefaultDisplayedValue("Destination");
+		JExtendedComboBox<Chauffeur> chauffeurBox = new JExtendedComboBox<Chauffeur>(agency.getAllChauffeurs());
+		JExtendedComboBox<Bus> busBox = new JExtendedComboBox<>(agency.getAllBusses());
+		toursNorthPanel.add(destiBox);
+		toursNorthPanel.add(chauffeurBox);
+		toursNorthPanel.add(busBox);
+
+		this.setLayout(new BorderLayout());
+		this.add(toursNorthPanel, BorderLayout.NORTH);
+	}
+}
