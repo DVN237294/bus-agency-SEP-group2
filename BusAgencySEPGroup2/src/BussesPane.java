97b8788ab39f0d94ed393f36e22612a8328c53b8
Afonso <Afonso@10.52.226.172>
2016-12-06 18:06:56 +0100

    Removed unused import

--- BusAgencySEPGroup2/src/BussesPane.java
+++ BusAgencySEPGroup2/src/BussesPane.java
@@ -2,7 +2,6 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
