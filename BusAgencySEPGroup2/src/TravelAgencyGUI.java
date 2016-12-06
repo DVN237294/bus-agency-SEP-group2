3e49a2f2e71dae20a0307699b8a00a2ab878ed1d
Afonso <Afonso@10.52.226.172>
2016-12-06 13:06:23 +0100

    Removed unused import

--- BusAgencySEPGroup2/src/TravelAgencyGUI.java
+++ BusAgencySEPGroup2/src/TravelAgencyGUI.java
@@ -1,5 +1,4 @@
 import java.awt.event.*;
-import java.awt.*;
 
 import javax.swing.*;
 
@@ -50,39 +49,11 @@ public class TravelAgencyGUI extends JFrame
 		}
 		
 		
-		//Tour pane
-		JPanel toursNorthPanel = new JPanel();
-		toursNorthPanel.setBorder(BorderFactory.createTitledBorder("Search"));
-		toursNorthPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-		String[] destinations = agency.getAllDestinations();
-		JExtendedComboBox<String> destiBox = new JExtendedComboBox<String>(destinations);
-		destiBox.setPrototypeDisplayValue("Destination");
-		destiBox.setDefaultDisplayedValue("Destination");
-		JExtendedComboBox<Chauffeur> chauffeurBox = new JExtendedComboBox<Chauffeur>(agency.getAllChauffeurs());
-		
-		toursNorthPanel.add(destiBox);	
-		toursNorthPanel.add(chauffeurBox);
-		tabPanels[0].setLayout(new BorderLayout());
-		tabPanels[0].add(toursNorthPanel, BorderLayout.NORTH);
+		//Tour pane		
+		tabPanels[0] = new ToursPane(agency);
 		tabbedPane.setComponentAt(0, tabPanels[0]);
 		
 		//Busses pane
-		JPanel bussesNorthPanel = new JPanel();
-      bussesNorthPanel.setBorder(BorderFactory.createTitledBorder("Reserve"));
-      bussesNorthPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-      JExtendedComboBox<Bus> busBox = new JExtendedComboBox<>(agency.getAllBusses());
-      destinations = agency.getAllDestinations();
-      destiBox = new JExtendedComboBox<String>(destinations);
-      destiBox.setPrototypeDisplayValue("Destination");
-      destiBox.setDefaultDisplayedValue("Destination");
-      chauffeurBox = new JExtendedComboBox<Chauffeur>(agency.getAllChauffeurs());
-      
-      bussesNorthPanel.add(destiBox);   
-      bussesNorthPanel.add(busBox);
-      bussesNorthPanel.add(chauffeurBox);
-      tabPanels[2].setLayout(new BorderLayout());
-      tabPanels[2].add(bussesNorthPanel, BorderLayout.NORTH);
-		
 		tabPanels[2] = new BussesPane(agency);
 		tabbedPane.setComponentAt(2, tabPanels[2]);
 		
