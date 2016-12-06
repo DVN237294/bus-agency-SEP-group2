04f6173dbdfd829ec6a0917e623e47e3356beb12
Afonso <Afonso@10.52.226.172>
2016-12-06 13:14:41 +0100

    Formatted code

--- BusAgencySEPGroup2/src/BussesPane.java
+++ BusAgencySEPGroup2/src/BussesPane.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 public class BussesPane extends JPanel
@@ -19,11 +20,11 @@ public class BussesPane extends JPanel
 	      bussesNorthPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 	      bussesWestPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
 	      JExtendedComboBox<Bus> busBox = new JExtendedComboBox<>(agency.getAllBusses());
-	      
 	      JExtendedComboBox<String> destiBox = new JExtendedComboBox<String>(agency.getAllDestinations());
 	      destiBox.setPrototypeDisplayValue("Destination");
 	      destiBox.setDefaultDisplayedValue("Destination");
 	      JExtendedComboBox<Chauffeur> chauffeurBox = new JExtendedComboBox<Chauffeur>(agency.getAllChauffeurs());
+	      
 	      
 	      bussesNorthPanel.add(destiBox);   
 	      bussesNorthPanel.add(busBox);
