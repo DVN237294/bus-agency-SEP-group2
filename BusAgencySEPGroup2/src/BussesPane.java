f22db184c60fd974d2c669984a83ca6b4be9ce0e
Afonso <Afonso@10.52.226.172>
2016-12-06 16:17:39 +0100

    improved serch "Engine" for the busses

--- BusAgencySEPGroup2/src/BussesPane.java
+++ BusAgencySEPGroup2/src/BussesPane.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -23,75 +24,78 @@ public class BussesPane extends JPanel
    private JButton searchButton;
    private JList<Bus> busList;
    private JButton deleteButton;
-   
+
    JPanel bussesNorthPanel = new JPanel();
    JPanel bussesWestPanel = new JPanel();
    JPanel bussesSouthPanel = new JPanel();
-   
-	public BussesPane(TravelAgency agency)
-	{
-		super();
-		this.agency = agency;
-		Bus[] allBusses = agency.getAllBusses();
-		
-		String[] makeArray = new String[allBusses.length];
-		String[] modelArray = new String[allBusses.length];
-		String[] licensePlateArray = new String[allBusses.length];
-		
-		   for (int i = 0; i < allBusses.length; i++)
-         {
-            makeArray[i] = allBusses[i].getMake();
-            modelArray[i] = allBusses[i].getModel();
-            licensePlateArray[i] = allBusses[i].getLicensePlate();
-         }
-		
-	      bussesNorthPanel.setBorder(BorderFactory.createTitledBorder("Search"));
-	      bussesNorthPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-	      bussesWestPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-	      destiBox = new JExtendedComboBox<String>(agency.getAllDestinations());
-	      destiBox.setPrototypeDisplayValue("Destination");
-	      destiBox.setDefaultDisplayedItem("Destination");
-	      makeBox = new JExtendedComboBox<>(makeArray);
-	      makeBox.setPrototypeDisplayValue("Make");
-	      makeBox.setDefaultDisplayedItem("Make");
-	      modelBox = new JExtendedComboBox<>(modelArray);
-	      modelBox.setPrototypeDisplayValue("Model");
-	      modelBox.setDefaultDisplayedItem("Model");
-	      licensePlateBox = new JExtendedComboBox<>(licensePlateArray);
-	      licensePlateBox.setPrototypeDisplayValue("License Plate");
-	      licensePlateBox.setDefaultDisplayedItem("License Plate");
-	      chauffeurBox = new JExtendedComboBox<Chauffeur>(agency.getAllChauffeurs());
-	      busList = new JList<Bus>(agency.getAllBusses());
-	      busList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-	      searchButton = new JButton("Search");
-	      deleteButton = new JButton("Delete");
-	      busList.setVisible(false);
-	      deleteButton.setVisible(false);
-	      
-	      bussesNorthPanel.add(destiBox);
-	      bussesNorthPanel.add(chauffeurBox);
-	      bussesNorthPanel.add(makeBox);
-	      bussesNorthPanel.add(modelBox);
-	      bussesNorthPanel.add(licensePlateBox);
-	      bussesWestPanel.add(busList);
-	      bussesNorthPanel.add(searchButton);
-         bussesSouthPanel.add(deleteButton);
-	      searchButton.addActionListener(new SearchAllListener());
-	      this.setLayout(new BorderLayout());
-	      this.add(bussesNorthPanel, BorderLayout.NORTH);
-	      this.add(bussesWestPanel, BorderLayout.WEST);
-	      this.add(bussesSouthPanel, BorderLayout.SOUTH);
-	}
-	
-	private class SearchAllListener implements ActionListener
-	{
+
+   public BussesPane(TravelAgency agency)
+   {
+      super();
+      this.agency = agency;
+      Bus[] allBusses = agency.getAllBusses();
+
+      String[] makeArray = new String[allBusses.length];
+      String[] modelArray = new String[allBusses.length];
+      String[] licensePlateArray = new String[allBusses.length];
+
+      for (int i = 0; i < allBusses.length; i++)
+      {
+         makeArray[i] = allBusses[i].getMake();
+         modelArray[i] = allBusses[i].getModel();
+         licensePlateArray[i] = allBusses[i].getLicensePlate();
+      }
+
+      bussesNorthPanel.setBorder(BorderFactory.createTitledBorder("Search"));
+      bussesNorthPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+      bussesWestPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+      destiBox = new JExtendedComboBox<String>(agency.getAllDestinations());
+      destiBox.setPrototypeDisplayValue("Destination");
+      destiBox.setDefaultDisplayedItem("Destination");
+      makeBox = new JExtendedComboBox<>(makeArray);
+      makeBox.setPrototypeDisplayValue("Make");
+      makeBox.setDefaultDisplayedItem("Make");
+      modelBox = new JExtendedComboBox<>(modelArray);
+      modelBox.setPrototypeDisplayValue("Model");
+      modelBox.setDefaultDisplayedItem("Model");
+      licensePlateBox = new JExtendedComboBox<>(licensePlateArray);
+      licensePlateBox.setPrototypeDisplayValue("License Plate");
+      licensePlateBox.setDefaultDisplayedItem("License Plate");
+      chauffeurBox = new JExtendedComboBox<Chauffeur>(agency.getAllChauffeurs());
+      searchButton = new JButton("Search");
+      deleteButton = new JButton("Delete");
+      busList = new JList<Bus>();
+      busList.setVisible(false);
+      deleteButton.setVisible(false);
+
+      bussesNorthPanel.add(destiBox);
+      bussesNorthPanel.add(chauffeurBox);
+      bussesNorthPanel.add(makeBox);
+      bussesNorthPanel.add(modelBox);
+      bussesNorthPanel.add(licensePlateBox);
+      bussesWestPanel.add(busList);
+      bussesNorthPanel.add(searchButton);
+      bussesSouthPanel.add(deleteButton);
+      searchButton.addActionListener(new SearchAllListener());
+      this.setLayout(new BorderLayout());
+      this.add(bussesNorthPanel, BorderLayout.NORTH);
+      this.add(bussesWestPanel, BorderLayout.WEST);
+      this.add(bussesSouthPanel, BorderLayout.SOUTH);
+   }
+
+   private class SearchAllListener implements ActionListener
+   {
 
       @Override
       public void actionPerformed(ActionEvent e)
       {
+         DefaultListModel<Bus> model = new DefaultListModel<>();
+         model.addElement(agency.getBus(makeBox.getSelectedItem(),
+               modelBox.getSelectedItem(), licensePlateBox.getSelectedItem()));
+         busList.setModel(model);
          busList.setVisible(true);
          deleteButton.setVisible(true);
       }
-	   
-	}
+
+   }
 }
