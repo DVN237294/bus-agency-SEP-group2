854c1945af2d29faf01582a54e98cca131633d9a
Afonso <Afonso@10.52.226.172>
2016-12-06 13:52:45 +0100

    Added list and delete button

--- BusAgencySEPGroup2/src/BussesPane.java
+++ BusAgencySEPGroup2/src/BussesPane.java
@@ -14,30 +14,52 @@ public class BussesPane extends JPanel
 {
    private static final long serialVersionUID = 1L;
    private TravelAgency agency;
+   private JExtendedComboBox<String> destiBox;
+   private JExtendedComboBox<Bus> busBox;
+   private JExtendedComboBox<Chauffeur> chauffeurBox;
+   private JButton searchButton;
+   private JList<Bus> busList;
+   private JButton deleteButton;
+   
+   JPanel bussesNorthPanel = new JPanel();
+   JPanel bussesEastPanel = new JPanel();
+   
 	public BussesPane(TravelAgency agency)
 	{
 		super();
 		this.agency = agency;
 		
-		JPanel bussesNorthPanel = new JPanel();
-		JPanel bussesEastPanel = new JPanel();
 	      bussesNorthPanel.setBorder(BorderFactory.createTitledBorder("Search"));
 	      bussesNorthPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 	      bussesEastPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-	      JExtendedComboBox<Bus> busBox = new JExtendedComboBox<>(agency.getAllBusses());
-	      JExtendedComboBox<String> destiBox = new JExtendedComboBox<String>(agency.getAllDestinations());
+	      busBox = new JExtendedComboBox<>(agency.getAllBusses());
+	      destiBox = new JExtendedComboBox<String>(agency.getAllDestinations());
 	      destiBox.setPrototypeDisplayValue("Destination");
 	      destiBox.setDefaultDisplayedValue("Destination");
-	      JExtendedComboBox<Chauffeur> chauffeurBox = new JExtendedComboBox<Chauffeur>(agency.getAllChauffeurs());
-	      JList<Bus> busList = new JList<Bus>(agency.getAllBusses());
-	      JButton searchButton = new JButton("Search");
+	      chauffeurBox = new JExtendedComboBox<Chauffeur>(agency.getAllChauffeurs());
+	      busList = new JList<Bus>(agency.getAllBusses());
+	      searchButton = new JButton("Search");
+	      deleteButton = new JButton("Delete");
 	      
 	      bussesNorthPanel.add(destiBox);   
 	      bussesNorthPanel.add(busBox);
 	      bussesNorthPanel.add(chauffeurBox);
 	      bussesNorthPanel.add(searchButton);
+	      searchButton.addActionListener(new SearchListener());
 	      this.setLayout(new BorderLayout());
 	      this.add(bussesNorthPanel, BorderLayout.NORTH);
 	      this.add(bussesEastPanel, BorderLayout.EAST);
+	}
+	
+	private class SearchListener implements ActionListener
+	{
+
+      @Override
+      public void actionPerformed(ActionEvent e)
+      {
+         bussesEastPanel.add(busList);
+         bussesEastPanel.add(deleteButton);
+      }
+	   
 	}
 }
